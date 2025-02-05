d325e0f1a62f27ef7e11d01925a32138e598553e
Chetan <Chetan@LAPTOP-D0QEJMKI>
2025-02-05 10:37:24 +0530

    second commit

---
+++ AutomationFramework/src/test/java/SynkFork/SynkFork.java
@@ -0,0 +1,11 @@
+package SynkFork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SynkFork {
+	@Test
+	public void synk() {
+		Reporter.log("synkFork class added", true);
+	}
+}
